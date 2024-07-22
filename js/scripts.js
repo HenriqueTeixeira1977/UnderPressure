4814804c9ea444575bfbb39e067835e5729e6272
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2024-07-22 10:50:08 -0300

    Update20240722105023

--- js/scripts.js
+++ js/scripts.js
@@ -24,10 +24,18 @@ closeBtn.addEventListener("click", () => {
 	menu.classList.remove('active');
 });
 
+
+
+
+
+
+
 //Script para Image Comparison Slider(Before / After)
+
+
 VanillaTilt.init(document.querySelector("#image-comparison-slider"), { // Tilt Effect - vanilla-tilt.js (https://micku7zu.github.io/vanilla-tilt.js/) is required for this
 	max: 9, // max tilt rotation (degrees (deg))
-	speed: 5, // speed (transition-duration) of the enter/exit transition (milliseconds (ms))
+	speed: 7, // speed (transition-duration) of the enter/exit transition (milliseconds (ms))
 	scale: 1.02 // transform scale - 2 = 200%, 1.5 = 150%, etc..
   });
 
@@ -41,18 +49,18 @@ VanillaTilt.init(document.querySelector("#image-comparison-slider"), { // Tilt E
   
   function sliderMouseMove(event) {
   
-	if(isSliderLocked) return;
-  
-	const sliderLeftX = slider.offsetLeft;
-	const sliderWidth = slider.clientWidth;
-	const sliderHandleWidth = sliderHandle.clientWidth;
-  
-	let mouseX = (event.clientX || event.touches[0].clientX) - sliderLeftX;
-	if(mouseX < 0) mouseX = 0;
-	else if(mouseX > sliderWidth) mouseX = sliderWidth;
-  
-	sliderImgWrapper.style.width = `${((1 - mouseX/sliderWidth) * 100).toFixed(4)}%`;
-	sliderHandle.style.left = `calc(${((mouseX/sliderWidth) * 100).toFixed(4)}% - ${sliderHandleWidth/2}px)`;
+    if(isSliderLocked) return;
+    
+    const sliderLeftX = slider.offsetLeft;
+    const sliderWidth = slider.clientWidth;
+    const sliderHandleWidth = sliderHandle.clientWidth;
+    
+    let mouseX = (event.clientX || event.touches[0].clientX) - sliderLeftX;
+    if(mouseX < 0) mouseX = 0;
+    else if(mouseX > sliderWidth) mouseX = sliderWidth;
+    
+    sliderImgWrapper.style.width = `${((1 - mouseX/sliderWidth) * 100).toFixed(4)}%`;
+    sliderHandle.style.left = `calc(${((mouseX/sliderWidth) * 100).toFixed(4)}% - ${sliderHandleWidth/2}px)`;
   }
   
   let isSliderLocked = false;
@@ -64,14 +72,14 @@ VanillaTilt.init(document.querySelector("#image-comparison-slider"), { // Tilt E
   slider.addEventListener("mouseleave", sliderMouseLeave);
   
   function sliderMouseDown(event) {
-	if(isSliderLocked) isSliderLocked = false;
-	sliderMouseMove(event);
+    if(isSliderLocked) isSliderLocked = false;
+    sliderMouseMove(event);
   }
   
   function sliderMouseUp() {
-	if(!isSliderLocked) isSliderLocked = true;
+    if(!isSliderLocked) isSliderLocked = true;
   }
   
   function sliderMouseLeave() {
-	if(isSliderLocked) isSliderLocked = false;
+    if(isSliderLocked) isSliderLocked = false;
   }
