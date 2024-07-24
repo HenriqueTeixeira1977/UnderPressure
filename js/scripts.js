f48ea6fcfae68400e9629fc6156d0b969205dc0c
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2024-07-24 14:40:42 -0300

    Update20240724144037

--- js/scripts.js
+++ js/scripts.js
@@ -29,47 +29,57 @@ VanillaTilt.init(document.querySelectorAll("#image-comparison-slider"), {
 	speed: 7, // speed (transition-duration) of the enter/exit transition (milliseconds (ms))
 	scale: 1.02 // transform scale - 2 = 200%, 1.5 = 150%, etc..
 });
-  
-const slider = document.querySelector("#image-comparison-slider");
-const sliderImgWrapper = document.querySelector("#image-comparison-slider .img-wrapper");
-const sliderHandle = document.querySelector("#image-comparison-slider .handle");
 
-slider.addEventListener("mousemove", sliderMouseMove);
-slider.addEventListener("touchmove", sliderMouseMove);
 
-function sliderMouseMove(event) {
+  /*  
+  const slider = document.querySelector("#image-comparison-slider");
+  const sliderImgWrapper = document.querySelector("#image-comparison-slider .img-wrapper");
+  const sliderHandle = document.querySelector("#image-comparison-slider .handle");
+  */
 
-  if(isSliderLocked) return;
-  
-  const sliderLeftX = slider.offsetLeft;
-  const sliderWidth = slider.clientWidth;
-  const sliderHandleWidth = sliderHandle.clientWidth;
-  
-  let mouseX = (event.clientX || event.touches[0].clientX) - sliderLeftX;
-  if(mouseX < 0) mouseX = 0;
-  else if(mouseX > sliderWidth) mouseX = sliderWidth;
-  
-  sliderImgWrapper.style.width = `${((1 - mouseX/sliderWidth) * 100).toFixed(4)}%`;
-  sliderHandle.style.left = `calc(${((mouseX/sliderWidth) * 100).toFixed(4)}% - ${sliderHandleWidth/2}px)`;
-}
+  document.querySelectorAll(".image-comparison-slider").forEach(slider => {
+    const sliderImgWrapper = slider.querySelector(".img-wrapper");
+    const sliderHandle = slider.querySelector(".handle");
 
-let isSliderLocked = false;
+    let isSliderLocked = false;
 
-slider.addEventListener("mousedown", sliderMouseDown);
-slider.addEventListener("touchstart", sliderMouseDown);
-slider.addEventListener("mouseup", sliderMouseUp);
-slider.addEventListener("touchend", sliderMouseUp);
-slider.addEventListener("mouseleave", sliderMouseLeave);
 
-function sliderMouseDown(event) {
-  if(isSliderLocked) isSliderLocked = false;
-  sliderMouseMove(event);
-}
+  slider.addEventListener("mousemove", sliderMouseMove);
+  slider.addEventListener("touchmove", sliderMouseMove);
 
-function sliderMouseUp() {
-  if(!isSliderLocked) isSliderLocked = true;
-}
+  function sliderMouseMove(event) {
+    if(isSliderLocked) return;
+    
+    const sliderLeftX = slider.offsetLeft;
+    const sliderWidth = slider.clientWidth;
+    const sliderHandleWidth = sliderHandle.clientWidth;
+    
+    let mouseX = (event.clientX || event.touches[0].clientX) - sliderLeftX;
+    if(mouseX < 0) mouseX = 0;
+    else if(mouseX > sliderWidth) mouseX = sliderWidth;
+    
+    sliderImgWrapper.style.width = `${((1 - mouseX/sliderWidth) * 100).toFixed(4)}%`;
+    sliderHandle.style.left = `calc(${((mouseX/sliderWidth) * 100).toFixed(4)}% - ${sliderHandleWidth/2}px)`;
+  }
 
-function sliderMouseLeave() {
-  if(isSliderLocked) isSliderLocked = false;
-}
+  //let isSliderLocked = false;
+
+  slider.addEventListener("mousedown", sliderMouseDown);
+  slider.addEventListener("touchstart", sliderMouseDown);
+  slider.addEventListener("mouseup", sliderMouseUp);
+  slider.addEventListener("touchend", sliderMouseUp);
+  slider.addEventListener("mouseleave", sliderMouseLeave);
+
+  function sliderMouseDown(event) {
+    if(isSliderLocked) isSliderLocked = false;
+    sliderMouseMove(event);
+  }
+
+  function sliderMouseUp() {
+    if(!isSliderLocked) isSliderLocked = true;
+  }
+
+  function sliderMouseLeave() {
+    if(isSliderLocked) isSliderLocked = false;
+  }
+});
