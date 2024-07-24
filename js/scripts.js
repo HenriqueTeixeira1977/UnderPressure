d70fa60b5ea2e2d2c901651a9cc8824ddd812297
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2024-07-23 23:57:14 -0300

    Update20240723235710

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,6 @@ window.addEventListener("scroll", function(){
 });
 
 
-
-
-
-
 //Script para Navigation sidebar menu responsivo
 
 var menu = document.querySelector('.menu');
@@ -26,60 +22,54 @@ closeBtn.addEventListener("click", () => {
 
 
 
-
-
-
-
 //Script para Image Comparison Slider(Before / After)
-
-
-VanillaTilt.init(document.querySelector("#image-comparison-slider"), { // Tilt Effect - vanilla-tilt.js (https://micku7zu.github.io/vanilla-tilt.js/) is required for this
+// Tilt Effect - vanilla-tilt.js (https://micku7zu.github.io/vanilla-tilt.js/) is required for this
+VanillaTilt.init(document.querySelectorAll("#image-comparison-slider"), { 
 	max: 9, // max tilt rotation (degrees (deg))
 	speed: 7, // speed (transition-duration) of the enter/exit transition (milliseconds (ms))
 	scale: 1.02 // transform scale - 2 = 200%, 1.5 = 150%, etc..
-  });
+});
+  
+const slider = document.querySelector("#image-comparison-slider");
+const sliderImgWrapper = document.querySelector("#image-comparison-slider .img-wrapper");
+const sliderHandle = document.querySelector("#image-comparison-slider .handle");
 
+slider.addEventListener("mousemove", sliderMouseMove);
+slider.addEventListener("touchmove", sliderMouseMove);
+
+function sliderMouseMove(event) {
+
+  if(isSliderLocked) return;
   
-  const slider = document.querySelector("#image-comparison-slider");
-  const sliderImgWrapper = document.querySelector("#image-comparison-slider .img-wrapper");
-  const sliderHandle = document.querySelector("#image-comparison-slider .handle");
+  const sliderLeftX = slider.offsetLeft;
+  const sliderWidth = slider.clientWidth;
+  const sliderHandleWidth = sliderHandle.clientWidth;
   
-  slider.addEventListener("mousemove", sliderMouseMove);
-  slider.addEventListener("touchmove", sliderMouseMove);
+  let mouseX = (event.clientX || event.touches[0].clientX) - sliderLeftX;
+  if(mouseX < 0) mouseX = 0;
+  else if(mouseX > sliderWidth) mouseX = sliderWidth;
   
-  function sliderMouseMove(event) {
-  
-    if(isSliderLocked) return;
-    
-    const sliderLeftX = slider.offsetLeft;
-    const sliderWidth = slider.clientWidth;
-    const sliderHandleWidth = sliderHandle.clientWidth;
-    
-    let mouseX = (event.clientX || event.touches[0].clientX) - sliderLeftX;
-    if(mouseX < 0) mouseX = 0;
-    else if(mouseX > sliderWidth) mouseX = sliderWidth;
-    
-    sliderImgWrapper.style.width = `${((1 - mouseX/sliderWidth) * 100).toFixed(4)}%`;
-    sliderHandle.style.left = `calc(${((mouseX/sliderWidth) * 100).toFixed(4)}% - ${sliderHandleWidth/2}px)`;
-  }
-  
-  let isSliderLocked = false;
-  
-  slider.addEventListener("mousedown", sliderMouseDown);
-  slider.addEventListener("touchstart", sliderMouseDown);
-  slider.addEventListener("mouseup", sliderMouseUp);
-  slider.addEventListener("touchend", sliderMouseUp);
-  slider.addEventListener("mouseleave", sliderMouseLeave);
-  
-  function sliderMouseDown(event) {
-    if(isSliderLocked) isSliderLocked = false;
-    sliderMouseMove(event);
-  }
-  
-  function sliderMouseUp() {
-    if(!isSliderLocked) isSliderLocked = true;
-  }
-  
-  function sliderMouseLeave() {
-    if(isSliderLocked) isSliderLocked = false;
-  }
+  sliderImgWrapper.style.width = `${((1 - mouseX/sliderWidth) * 100).toFixed(4)}%`;
+  sliderHandle.style.left = `calc(${((mouseX/sliderWidth) * 100).toFixed(4)}% - ${sliderHandleWidth/2}px)`;
+}
+
+let isSliderLocked = false;
+
+slider.addEventListener("mousedown", sliderMouseDown);
+slider.addEventListener("touchstart", sliderMouseDown);
+slider.addEventListener("mouseup", sliderMouseUp);
+slider.addEventListener("touchend", sliderMouseUp);
+slider.addEventListener("mouseleave", sliderMouseLeave);
+
+function sliderMouseDown(event) {
+  if(isSliderLocked) isSliderLocked = false;
+  sliderMouseMove(event);
+}
+
+function sliderMouseUp() {
+  if(!isSliderLocked) isSliderLocked = true;
+}
+
+function sliderMouseLeave() {
+  if(isSliderLocked) isSliderLocked = false;
+}
